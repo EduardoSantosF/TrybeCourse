8ab0cbf8ac79ba112c03bf1846a5cc3bac654073
Eduardo Santos <eduardo.debest81@gmail.com>
2020-05-06 18:44:42 -0300

    base bonus2

---
+++ exercises/8/8_4/bonus2.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+// escreva a função removeMiddle para passar nos testes abaixo:
+
+const words = ['mouse', 'giraffe', 'queen', 'window', 'bottle'];
+const expectedWords = ['mouse', 'giraffe', 'window', 'bottle'];
+const expectedOutput = ['queen'];
+const output = removeMiddle(words);
+
+assert.deepEqual(output, expectedOutput);
+assert.deepEqual(words, expectedWords);
