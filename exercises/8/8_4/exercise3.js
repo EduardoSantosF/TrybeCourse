a35750fc53c7eba1435f5c946035223c28408a1f
Eduardo Santos <eduardo.debest81@gmail.com>
2020-05-06 16:23:17 -0300

    exercise 3 done in 50s

--- exercises/8/8_4/exercise3.js
+++ exercises/8/8_4/exercise3.js
@@ -1,6 +1,12 @@
 const assert = require('assert');
 // escreva a função addAllnumbers para passar nos testes abaixo:
-
+function addAllnumbers(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;
+}
 //
 const numbers = [9, 23, 10, 3, 8];
 const expected = 53;
