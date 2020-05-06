2377782a69f226ba11ab45b3ad297985022122c8
Eduardo Santos <eduardo.debest81@gmail.com>
2020-05-06 18:36:37 -0300

    exercise 7 done in 2:20 min in the seccond attempt

--- exercises/8/8_4/exercise8.js
+++ exercises/8/8_4/exercise8.js
@@ -1,12 +1,15 @@
 function secondThirdSmallest(array) {
   let results = []
-  array.sort(function (x, y) {
-      return x + y;
+  array.sort((x, y) => {
+    if (x > y) {
+      return 1;
+    }
+    return 0;
   });
   results = [array[1], array[2]];
   return results;
 };
-
+const assert = require('assert');
 const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
 const result = [5, 6];
 /*
@@ -16,3 +19,4 @@ e, caso não esteja, altere o código para que ele passe nos testes.
 Lembre-se: testes pequenos e numerosos! Escreva um por vez e vá corrigindo
 a função aos poucos:
 */
+assert.deepEqual(secondThirdSmallest(parameter), result);
